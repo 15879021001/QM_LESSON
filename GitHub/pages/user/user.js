77107976f0e70f45f3d6dae3b1c00769057ef76e
Too-Tao <599366020@qq.com>
2018-10-13 21:17:37 +0800

    微信小程序，数据的访问

---
+++ GitHub/pages/user/user.js
@@ -0,0 +1,39 @@
+Page({
+    data: {
+      username: ''
+    },
+    userNameInput (e) {
+      console.log(e.detail.value);
+      this.setData({
+        username: e.detail.value
+      })
+    },
+    toInfo () {
+      // console.log(this.data.username)
+      let isNav = false
+      if (this.data.username) {
+        wx.showToast({
+          title: '数据加载中...',
+          icon: 'loading',
+          duration: 1000
+        });
+      } else {
+        wx.showModal({
+          title: '请输入用户名...',
+          showCancel: false,
+          success: function(res) {
+          }
+        })
+      }
+    //   存储
+      wx.setStorage({
+          key: 'username',
+          data: this.data.username,
+          
+      })
+    //   设置跳转时间
+      setTimeout(() => wx.switchTab({
+        url: '../index/index'
+      }), 1000)
+    }
+  })
