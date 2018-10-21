29317fabfc2d0f08b3f8db6e438b7cdc95453ff4
Too-Tao <599366020@qq.com>
2018-10-21 19:18:43 +0800

    Easy mock 的使用

--- ikcamp/pages/index/index.js
+++ ikcamp/pages/index/index.js
@@ -1,5 +1,8 @@
 //index.js
 //获取应用实例
+import config from '../../utils/config'
+
+
 const app = getApp()
 
 Page({
@@ -17,48 +20,42 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    this.requestArticle();
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
 
-      })
-    }
+
+  showDetail:function(e){
+    let dataset = e.currentTarget.dataset;
+    let item = dataset && dataset.item;
+    let contentId = item.contentId || 0;
+    wx.navigateTo({
+      url:`../detail/detail?contentId=${contentId}`
+    });
   },
-  requestArticle: function() {
-      wx.request({
+
+  onLoad: function () {
+    
+    let title = config.defaultBarTitle
+    wx.setNavigationBarTitle({
+      title,
+      success:function(res){
+
+      }
+    })
+    
+    this.requestArticle();
+    
+  },
+  requestArticle: function () {
+    wx.request({
       url: 'https://www.easy-mock.com/mock/5bca924ae6742c1bf8220bdb/ikcamp/list#!method=get',
       success: (res) => {
         console.log(res.data.data);
         let list = this.data.articleList;
-        list = [...list,...res.data.data];//...用来展开数组 [[1,2,3],4,5,6] 变成 [1,2,3,4,5,6]
+        list = [...list, ...res.data.data]; //...用来展开数组 [[1,2,3],4,5,6] 变成 [1,2,3,4,5,6]
         console.log(list);
 
         list.push(res.data.data);
@@ -70,8 +67,8 @@ Page({
         })
       }
     })
-    },
-  getUserInfo: function(e) {
+  },
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
