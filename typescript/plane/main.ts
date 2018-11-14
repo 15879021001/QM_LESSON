490396ff9b659166b3f883fa8970bd71c7c73400
Too-Tao <599366020@qq.com>
2018-11-14 16:29:01 +0800

    typescript 和 小游戏框架egret

---
+++ typescript/plane/main.ts
@@ -0,0 +1,18 @@
+class Main extends egret.DisplayObjectContainer {
+    constructor() {
+        super();
+
+        this.addEventListener(egret.Event.ADDED_TO_STAGE,this.onAddToStage,this);
+    }
+    onAddToStage(event){
+        let textField = new egret.TextField();
+        this.addChild(textField);
+        textField.textColor = 0xff0000;
+        textField.text = "hello world";
+    }
+}
+
+
+
+window['Main'] = Main;
+egret.runEgret();
---
+++ typescript/plane/readme.md
@@ -0,0 +1,20 @@
+1. 命名空间
+    egret 命名空间
+        .event.ADDED_TO_STAGE 划分引擎的系统 协作开发
+
+        地图 会话 中将 装备...
+        var egret = {};
+        egret.Event = {};
+        egret.Event.ADDED_TOSTAGE = function(){};
+        egret.DisplayObjectContainer = class{} 
+        egret.TextField = class{}
+    window 全局
+
+2. 静态类型语言 C++ java
+    - main()
+    - 类型检查
+    - 先编译再运行
+
+3. typescript 是 JS 的超集
+    
+    declare var egret:any
