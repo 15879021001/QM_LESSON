a40a21e8eb3af1de51ad4f90e633d2cc46245060
Too-Tao <599366020@qq.com>
2018-11-20 14:47:48 +0800

    wepy

---
+++ wepy-shop/src/api/api.js
@@ -0,0 +1,9 @@
+import{
+    wxRequest
+} from  "@/utils/wxRequest";
+
+const apiMall = 'https://sujiefs.com'
+const getAdList = (params)=> wxRequest(params,apiMall + '/api/adverts/list')
+export default{
+    getAdList
+}
