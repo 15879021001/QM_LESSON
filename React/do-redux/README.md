6d4108e001a835d2fe948982b25419b85a990f43
Too-Tao <599366020@qq.com>
2018-12-27 17:47:20 +0800

    手撸redux源码

---
+++ React/do-redux/README.md
@@ -0,0 +1,4 @@
+- redux 让所有状态共享， 可读 但不能随意写 只有action可以修改state 通过执行reducer
+  组件只能dispatch 一个action
+
+- reducer 不可直接调用，通过action才可以调用
---
+++ React/do-redux/src/redux/state.js
@@ -0,0 +1,10 @@
+export const state = {
+  head: {
+    text: 'head',
+    color: 'red'
+  },
+  body: {
+    text: 'body',
+    color: 'green'
+  }
+}
