c42407cc96060c5923e804e95528b9f7eb8b33b1
Too-Tao <599366020@qq.com>
2019-01-25 16:55:01 +0800

    初次使用egg.js egg.js入门

---
+++ egg/egg-example/README.md
@@ -0,0 +1,8 @@
+- node view 需要编译
+  router request => controller 函数（类） await 数据
+  UI tpl 在view/*.tpl html render
+
+  viewEngine tpl 模板的语法 nunjucks
+
+  使用前需先安装
+  egg-view-nunjucks
---
+++ egg/egg-example/config/config.default.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1548404579331_5933';
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks'
+    }
+  }
+  // add your config here
+  config.middleware = [];
+
+  return config;
+};
